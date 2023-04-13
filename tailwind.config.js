9b15f634213345d979995aa13b817dc40e7a101e
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-04-13 13:16:46 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      brand_color_red: "#EB3850",
+      from_gradient_color_red: "#A93E4C",
+      to_gradient_color_red: "#EB3850",
+      from_gradient_color_yellow: "#D6AE7B",
+      to_gradient_color_yellow: "#E3C79E",
+      from_gradient_color_dark: "#29323C",
+      to_gradient_color_dark: "#485563",
+      dark_10: "#1A191E",
+      dark_20: "#1A191E",
+    },
+  },
+  plugins: [],
+};
